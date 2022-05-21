08162f786f3a13bf739aa0132339cbf7ea409076
GamePlayer-8 <gameplayer2019pl@tutamail.com>
2022-05-21 20:39:27 +0100

    Patch

--- termux-app/terminal-emulator/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs-debug/termux/termux.o.d
+++ termux-app/terminal-emulator/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs-debug/termux/termux.o.d
@@ -1,2 +1,2 @@
-/home/gameplayer/workspace/termux-0.83-compiles/termux-app/terminal-emulator/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs-debug/termux/termux.o: \
-  /home/gameplayer/workspace/termux-0.83-compiles/termux-app/terminal-emulator/src/main/jni/termux.c
+/mnt/data/gameplayer/workspace/termux-0.83-compiles/termux-app/terminal-emulator/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs-debug/termux/termux.o: \
+  /mnt/data/gameplayer/workspace/termux-0.83-compiles/termux-app/terminal-emulator/src/main/jni/termux.c
